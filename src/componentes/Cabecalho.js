7f07249b489ec0b00daf5aeefd809e0f25936795
wellington <well@gmail.com>
2023-03-02 20:21:40 -0300

    criando o h3 em cabeçalho

--- src/componentes/Cabecalho.js
+++ src/componentes/Cabecalho.js
@@ -3,9 +3,14 @@ import React from 'react'
 export default function Cabecalho(props){
 
 return(
-    <header className='headerC'>
+    <>
+     <header className='headerC'>
         <h1>{props.tituloCab}</h1>
         <p>{props.parag}</p>
     </header>
+    <h3>Home</h3>
+    
+    </>
+   
 )
 }
